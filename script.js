d7baa4821bbe43af8c3391fb3c5fedd6363f025d
@joseavera <69088516+joseavera@users.noreply.github.com>
2025-06-11 15:38:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
   const firebaseConfig = {
     apiKey: "AIzaSyBZEVISZuRfn1iC3be5b7xTMQiCZubXyTU",
     authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "bdnosql-de619",
-    storageBucket: "bdnosql-de619.firebasestorage.app",
-    messagingSenderId: "519369652548",
-    appId: "1:519369652548:web:260c2b45275f5a0ff5ff11",
-    measurementId: "G-1FYEJLG220"
+
   };
 
 // Inicializa Firebase
